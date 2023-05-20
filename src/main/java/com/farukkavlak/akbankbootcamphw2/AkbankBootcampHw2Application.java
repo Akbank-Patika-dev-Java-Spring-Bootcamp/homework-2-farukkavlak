93136f8714f45663ff69206ce160268d3f710d8a
Ömer Faruk Kavlak <ofaruk.kavlak@gmail.com>
2023-05-21 00:41:44 +0300

    Main structure

---
+++ src/main/java/com/farukkavlak/akbankbootcamphw2/AkbankBootcampHw2Application.java
@@ -0,0 +1,13 @@
+package com.farukkavlak.akbankbootcamphw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbankBootcampHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbankBootcampHw2Application.class, args);
+	}
+
+}
